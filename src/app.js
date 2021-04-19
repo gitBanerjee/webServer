c84c8a96186761822f7ded5cf71939a4e971d4ae
gitBanerjee <sourav.itsme777@gmail.com>
2021-04-19 14:39:46 +0530

    Added Readme file

--- src/app.js
+++ src/app.js
@@ -1,14 +1,48 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-app.get('',(req,res) =>{
-    res.send('Hello Express')
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('',(req,res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Sourav Banerjee'
+    })
+})
+
+app.get('/about',(req,res) => {
+    res.render('about', {
+        title:'About',
+        name:'Sourav Banerjee'
+    })
 })
 
 app.get('/help',(req,res) => {
-    res.send('Help Page')
+    res.render('help', {
+        title:'Help',
+        name:'Sourav Banerjee'
+    })
 })
+
+app.get('/weather',(req,res) => {
+    res.render('Your weather')
+})
+
+
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
